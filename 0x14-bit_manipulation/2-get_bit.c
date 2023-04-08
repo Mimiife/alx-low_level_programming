dfee63d0ad83583978fce0cc4c372cf89c799bae
Mimiife <ifekoyamary49@gmail.com>
2023-04-08 15:32:19 -0700

    correction of 2-get_bit.c file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,11 +11,18 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int dec_bit;
-
-	if (index > 63)
+	if (index >= 64)
 		return (-1);
 
-	dec_bit = (n >> index) & 1;
+	n = n >> index;
 
-	return (dec_bit);
+	if ((n & 1) == 1)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
