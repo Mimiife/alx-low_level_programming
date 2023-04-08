70f7fa17c9105b3d903bc4b2a4e455dcf902617a
Mimiife <ifekoyamary49@gmail.com>
2023-04-08 08:20:14 -0700

    update of task 0x15. C - file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,26 +12,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int x = 0, file;
+	int fd, w, buffer = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
-
-	while (text_content[i] != '\0')
+	if (text_content != NULL)
 	{
-		x++;
+		for (buffer = 0; text_content[buffer];)
+			buffer++;
 	}
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, buffer);
 
-	if (file == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	write(file, text_content, x);
+	close(fd);
 
 	return (1);
 }
+
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,38 +1,34 @@
-[200~#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- *  * append_text_to_file - Appends text at the end of a file.
- *   * @filename: A pointer to the name of the file.
- *    * @text_content: The string to add to the end of the file.
- *     * Description: function that appends text at the end of a file
- *      * Return: 1 on success, -1 on failure
- *       */
+ * append_text_to_file - that appends text at the end of a file
+ * @filename: A pointer to the name of the file
+ * @text_content: content file
+ * Description: function that appends text at the end of a file
+ * Return: 1 on success, -1 on failure
+ */
 
 int append_text_to_file(const char *filename, char *text_content)
-
 {
-	int x = 0, file;
+	int o, w, buffer = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
-
-	while (text_content[x] != '\0')
+	if (text_content != NULL)
 	{
-		x++;
+		for (buffer = 0; text_content[buffer];)
+			buffer++;
 	}
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, buffer);
 
-	if (file == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	write(file, text_content, x);
+	close(o);
 
 	return (1);
 }
